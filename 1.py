1cde5263a07c1c4a4e890aecc18e9440d4e08429
Kirill <motkirillpostman@gmail.com>
2024-07-21 19:57:16 +0300

    1

---
+++ 1.py
@@ -0,0 +1,13 @@
+import requests
+# url = 'https://melbet-156973.top/ru/toto/fifteen'
+url = 'https://melbet-156973.top/ru/toto/fifteen/draw/3538/1'
+header = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+}
+p = requests.get(url, headers=header)
+src = p.text
+# print(p)
+with open('text.html', 'w', encoding='utf-8') as f:
+    # print(p)
+    f.write(src)
