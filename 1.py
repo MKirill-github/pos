185bb20e9311199d5d5068a19aa22751ad171c5b
Kirill <motkirillpostman@gmail.com>
2024-07-21 20:24:15 +0300

    1

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import requests
-# url = 'https://melbet-156973.top/ru/toto/fifteen'
-url = 'https://melbet-156973.top/ru/toto/fifteen/draw/3538/1'
+url = 'https://melbet-156973.top/ru/toto/fifteen'
+# url = 'https://melbet-156973.top/ru/toto/fifteen/draw/3538/1'
 header = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
