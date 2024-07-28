49c4c26dddea3ed2411c035b01820df0d1f9e222
Kirill <motkirillpostman@gmail.com>
2024-07-28 21:42:41 +0300

    2142

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 url = 'https://melbet-156973.top/ru/toto/fifteen'
 # url = 'https://melbet-156973.top/ru/toto/fifteen/draw/3538/1'
+url_3 = "C:\Users\CL\Desktop\Python\pos\1.html"
 header = {
     'Accept': '*/*',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
